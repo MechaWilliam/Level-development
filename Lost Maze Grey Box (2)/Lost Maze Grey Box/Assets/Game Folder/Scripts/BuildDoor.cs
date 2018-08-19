70cbfd74db9e5a81ece111663788d2626f9d7910
MechaWilliam <40289865+MechaWilliam@users.noreply.github.com>
2018-08-19 22:52:43 +0800

    23
    23

---
+++ Lost Maze Grey Box (2)/Lost Maze Grey Box/Assets/Game Folder/Scripts/BuildDoor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildDoor : MonoBehaviour
+{
+    public GameObject pieceOne;
+    public GameObject pieceTwo;
+    public GameObject pieceThree;
+    public GameObject pieceFour;
+
+  
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Gold")
+        {
+            pieceOne.SetActive(true);
+        }
+        else if (other.gameObject.tag == "Silver")
+        {
+            pieceTwo.SetActive(true);
+        }
+        else if (other.gameObject.tag == "Blue")
+        {
+            pieceThree.SetActive(true);
+        }
+        else if (other.gameObject.tag == "Red")
+        {
+            pieceFour.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Gold")
+        {
+            pieceOne.SetActive(false);
+        }
+        else if (other.gameObject.tag == "Silver")
+        {
+            pieceTwo.SetActive(false);
+        }
+        else if (other.gameObject.tag == "Blue")
+        {
+            pieceThree.SetActive(false);
+        }
+        else if (other.gameObject.tag == "Red")
+        {
+            pieceFour.SetActive(false);
+        }
+    }
+
+
+}
---
+++ Lost Maze Grey Box (2)/Lost Maze Grey Box/Assets/Game Folder/Scripts/Enenmycontroller.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Enenmycontroller : MonoBehaviour
+{
+    public enum EnemyState
+    {
+        Patrol,
+        Chase
+    }
+    //Variables managing enemy waypoint system
+    public NavMeshAgent agent;
+    public Transform[] waypoints;
+    public float targetThreshold = 1.5f;
+    private int currentWaypointIndex = 0;
+    private Transform target;
+
+    //Variables managing enemy chase
+    private GameObject player;
+    public float searchRadius = 2;
+    public float searchAngle = 60;
+
+    private EnemyState state = EnemyState.Patrol;
+
+    void Start()
+    {
+        //Locate the player inside the game scene
+        player = GameObject.Find("Player");
+        //Setup initial target based on the first waypoint
+        target = waypoints[0];
+    }
+
+    void Update()
+    {
+        //If the player is within the search radius and search angle, chase them
+        if ((transform.position - player.transform.position).magnitude <= searchRadius && WithinViewRange())
+            state = EnemyState.Chase;
+        //If not, patrol around the set waypoints
+        else
+            state = EnemyState.Patrol;
+
+        //Run state functions
+        if (state == EnemyState.Chase)
+            RunChase();
+        else if (state == EnemyState.Patrol)
+            RunPatrol();
+    }
+
+    void RunChase()
+    {
+        //Assign player position as enemy destination if chasing
+        agent.SetDestination(player.transform.position);
+    }
+
+    void RunPatrol()
+    {
+        //Do not patrol if there are less than 2 waypoints
+        if (waypoints.Length < 2)
+            return;
+
+        agent.SetDestination(target.position);
+        //If the enemy has reached the current waypoint, iterate to the next waypoint
+        if ((transform.position - target.position).magnitude <= targetThreshold)
+        {
+            UpdateWaypoint();
+        }
+    }
+
+    void UpdateWaypoint()
+    {
+        currentWaypointIndex++;
+        if (currentWaypointIndex >= waypoints.Length)
+        {
+            currentWaypointIndex = 0;
+        }
+
+        target = waypoints[currentWaypointIndex];
+    }
+
+    bool WithinViewRange()
+    {
+        //Use dot product to determine if the player is currently within the enemys vision, specified by searchAngle
+
+        //First normalise the enemys heading vector, and the vector pointing from the enemy to the player
+        Vector3 normalisedHeading = transform.forward.normalized;
+        Vector3 normalisedPlayerVector = (player.transform.position - transform.position).normalized;
+        //Determine the dotproduct of the two normalised vectors
+        float dotProduct = Vector3.Dot(normalisedHeading, normalisedPlayerVector);
+        //Convert the dot product into an angle in degrees
+        float angleToPlayer = Mathf.Acos(dotProduct) * Mathf.Rad2Deg;
+
+        if (angleToPlayer > searchAngle)
+            return false;
+        else
+            return true;
+    }
+}
